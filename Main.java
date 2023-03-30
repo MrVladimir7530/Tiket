c4b0775a8e9cdf89b4427ef2fb29d25b727272ca
MrVladimir <vladimir.volkov-2013@yandex.ru>
2023-03-30 15:18:18 +0300

    First commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String st1 = "Ivan";
+        String st2 = new String("Ivan");
+        String st3 = new String("Ivan");
+    }
+}
